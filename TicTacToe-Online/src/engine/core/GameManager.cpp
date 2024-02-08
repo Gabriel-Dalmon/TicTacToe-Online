b1e1ae368d1a89e4ca7653ffc535be666d6f1e5f
Gabriel <gabriel@3dverse.com>
2024-02-08 01:25:03 +0100

    basic flags setup for GameManager->switchGameState

--- TicTacToe-Online/src/engine/core/GameManager.cpp
+++ TicTacToe-Online/src/engine/core/GameManager.cpp
@@ -9,9 +9,9 @@ GameManager* GameManager::instance_ = NULL;
 
 
 
-void GameManager::InitGameManager(Window* pWindow, GameState* initialGameState) {
+void GameManager::InitGameManager(Window* pWindow, GameState* pInitialGameState) {
     this->window = pWindow;
-    this->gameState = initialGameState;
+    this->gameState = pInitialGameState;
 }
 
 GameManager& GameManager::instance() 
@@ -62,8 +62,19 @@ GameState* GameManager::getGameState()
     return this->gameState;
 }
 
-void GameManager::switchGameState(GameState* gameState, destroyPreviousGameState = )
+void GameManager::switchGameState(GameState* gameState, SwitchGameStateFlags eSwitchGameStateFlags = (SwitchGameStateFlag)0)
 {
+    if (eSwitchGameStateFlags & COPY_GAME_OBJECTS) {
+        gameState->copyGameObjects(m_GameState);
+        eSwitchGameStateFlags &= ~DELETE_GAME_OBJECTS;
+    }
+    if (eSwitchGameStateFlags & DELETE_GAME_OBJECTS) {
+        m_GameState->deleteAllGameObjects();
+    }
+    if (eSwitchGameStateFlags & DELETE_OLD_GAMESTATE) {
+		delete m_GameState;
+	}
+
     std::cout << "Switching game state" << std::endl;
     this->gameState = gameState;
 }
--- TicTacToe-Online/src/engine/core/GameManager.h
+++ TicTacToe-Online/src/engine/core/GameManager.h
@@ -13,17 +13,25 @@ namespace sf
 	class Mouse;
 }
 
+namespace SwitchGameStateFlags {
+	const int DELETE_OLD_GAMESTATE = 1 << 0;  // 0001
+	const int DELETE_GAME_OBJECTS = 1 << 1;   // 0010
+	const int COPY_GAME_OBJECTS = 1 << 2;     // 0100
+}
+
+namespace SwitchGameStateFlag = SwitchGameStateFlags;
+
 class GameManager
 {
 public:
 	~GameManager() {};
 	static GameManager& instance();
 
-	void InitGameManager(Window* pWindow, GameState* initialGameState);
+	void InitGameManager(Window* pWindow, GameState* pInitialGameState);
 	void render();
 	void update();
 	GameState* getGameState();
-	void switchGameState(GameState*);
+	void switchGameState(GameState* pGameState, int iSwitchGameStateFlags);
 
 
 	EventsManager* getEventsManager();
