4be8a2b915ac6fe8e9a192b92c02a75e9fb70493
bboismenu <bboismenu@gaming.tech>
2024-02-14 08:22:47 +0100

    Temporary for merge

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -7,6 +7,14 @@
 #include "socketRequirements.h"
 #include "Thread.h"
 
+enum matchStates
+{
+    waiting_players = 0,
+    running,
+    player1Win,
+    player2Win,
+    tie
+};
 
 
 //int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -26,8 +34,21 @@ int main(int argc, char** argv)
 
     SOCKET Listen = sockWinPair.first;
     HWND Window = sockWinPair.second;*/
-    while (true) {
-        //
+    while (true)
+    {
+        // Intialize everything the server locally needs to keep track of players and the match
+        int grid[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        char* playersName[2];
+        SOCKET* turnOf;
+        int matchState = waiting_players;
+        bool noClients = false;
+
+        // Enter main server loop. If no clients are connected, a timer starts, and upon reaching XX, the server closes.
+        while (!noClients /*&& timer is below threashold*/)
+        {   
+            // By default, it will do nothing until it receives an event
+        }
+            
     }
 
 }
